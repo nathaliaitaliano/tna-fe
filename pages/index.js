1cb17e22d838b93fb41a2806f1cc7fe2db86e172
Fernando <fernandoasfj+github@gmail.com>
2021-03-11 18:36:02 +0100

    Basic iteraction

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Head from 'next/head'
+import { useState } from 'react';
 import styles from '../styles/Chat.module.css'
 
 export default function Chat() {
+
+  const [history, setHistory] = useState(["Hello, type something"])
+  const [currentMessage, setCurrentMessage] = useState("")
+  const messageHistory = history.map((msg, index) => <p key={index} className={index % 2 == 0 ? styles.leftMessage : styles.rightMessage}>{msg}</p>)
+
+  const updateHistory = () => {
+    setHistory([...history, currentMessage])
+    setCurrentMessage("")
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,10 +22,12 @@ export default function Chat() {
 
       <main className={styles.main}>
         <h1>Chat</h1>
-        <div className={styles.display}></div>
+        <div className={styles.display}>
+          {messageHistory}
+        </div>
         <div className={styles.submitMessage}>
-          <input type="text" className={styles.message}></input>
-          <button className={styles.button}>SEND</button>
+          <input type="text" value={currentMessage} className={styles.message} onChange={e => setCurrentMessage(e.target.value)}></input>
+          <button className={styles.button} onClick={updateHistory}>SEND</button>
         </div>
       </main>
 
