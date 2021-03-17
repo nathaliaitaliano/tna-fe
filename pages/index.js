17bfaa209423e9db6602634d38bcbf6a764b3362
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-17 11:13:13 +0100

    information add to footer

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Chat() {
       </main>
 
       <footer className={styles.footer}>
-        <p> By <span><a href="https://github.com/fernando-alves">Fernando Alves</a></span> and <span><a href="https://github.com/nathaliaitaliano">Nathalia Italiano</a></span></p>
+        <p> By <span><a href="https://github.com/fernando-alves">Fernando Alves</a></span> and <span><a href="https://github.com/nathaliaitaliano">Nathalia Italiano</a></span> 🤍</p>
       </footer>
     </div>
   )
