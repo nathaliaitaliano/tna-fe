9bfee8634a07ba934abfed55125413bcf08f3201
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-20 21:47:50 +0100

    refactor to use reducer intead of useState

--- components/chat/chat.js
+++ components/chat/chat.js
@@ -1,39 +1,43 @@
 
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useReducer } from 'react';
 import styles from './chat.module.css'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'messageSubmitted':
+      return { history: [...state.history, action.message] }
+  }
+}
 
 export default function Chat() {
 
-  const [history, setHistory] = useState(["Hello, type something"])
+  const [state, dispatch] = useReducer(reducer, { history: ["Hello, type something here!"] })
+
   const [currentMessage, setCurrentMessage] = useState("")
   const chatBottom = useRef(null)
-  const messageHistory = history.map((msg, index) => <p key={index} className={index % 2 == 0 ? styles.userMessage : styles.botResponse}>{msg}</p>)
+  const messageHistory = state.history.map((msg, index) => <p key={index} className={index % 2 == 0 ? styles.userMessage : styles.botResponse}>{msg}</p>)
 
-  const updateHistory = () => {
-    setHistory([...history, currentMessage])
+  const submitMessage = () => {
+    dispatch({ type: 'messageSubmitted', message: currentMessage })
     setCurrentMessage("")
   }
 
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      updateHistory();
-    }
-  }
+  const handleKeyPress = (event) => event.key === "Enter" && submitMessage()
 
   useEffect(() => {
     chatBottom.current?.scrollIntoView({ behavior: "smooth" })
-  }, [history]);
+  }, [state.history]);
 
   return (
-        <div className={styles.chatBot}>
-          <div className={styles.display}>
-            {messageHistory}
-            <div ref={chatBottom}></div>
-          </div>
-          <div className={styles.submitMessage}>
-            <input className={styles.inputMessage} type="text" autoComplete="off" value={currentMessage} onKeyPress={handleKeyPress} onChange={e => setCurrentMessage(e.target.value)}></input>
-            <button className={styles.btnSendMessage} onClick={updateHistory}>SEND <i className={styles.sendIcon}></i></button>
-          </div>
-        </div>
+    <div className={styles.chatBot}>
+      <div className={styles.display}>
+        {messageHistory}
+        <div ref={chatBottom}></div>
+      </div>
+      <div className={styles.submitMessage}>
+        <input className={styles.inputMessage} type="text" autoComplete="off" value={currentMessage} onKeyPress={handleKeyPress} onChange={e => setCurrentMessage(e.target.value)}></input>
+        <button className={styles.btnSendMessage} onClick={submitMessage}>SEND <i className={styles.sendIcon}></i></button>
+      </div>
+    </div>
   )
 }
